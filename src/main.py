b4f4113072c0178e01b3f9618e513495b8853701
Alexander Gedz <gedz.buh@yandex.ru>
2024-03-01 11:09:38 +0300

    fix main

--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 class Category:
     total_categories = 0
-    unique_products = set()
+    unique_products = 0
 
     def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
         self.products = products
-        Category.total_categories += 1
 
-        for product in products:
-            Category.unique_products.add(product)
+        Category.total_categories += 1
+        Category.unique_products += 1
+
+    def get_name(self):
+        return self.name
+
+    def get_description(self):
+        return self.description
+
+    def get_products(self):
+        return self.products
 
 
 class Product:
@@ -19,4 +27,14 @@ class Product:
         self.price = price
         self.quantity = quantity
 
+    def get_product_name(self):
+        return self.name
 
+    def get_product_description(self):
+        return self.description
+
+    def get_product_price(self):
+        return self.price
+
+    def get_product_quantity(self):
+        return self.quantity
